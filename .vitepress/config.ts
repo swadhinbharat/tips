7b019f5d8beab1a1b199a912d4124a034d4d239a
Swadhin Patel <me@swadh.in>
2024-07-10 13:46:56 +0800

    chore: Add base URL for GitHub Pages deployment

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Tips",
   description: "Tips and Tricks",
+  base: '/repo/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
